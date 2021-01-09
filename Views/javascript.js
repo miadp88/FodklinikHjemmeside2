845bbb0b38d598fe080354efc5818e745080ab19
miadp88 <54668407+miadp88@users.noreply.github.com>
2021-01-09 11:06:58 +0100

    fixing nav by centering and setting backgroundcolor

--- Views/javascript.js
+++ Views/javascript.js
@@ -73,20 +73,23 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  if (slides.length > 0){
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
 
 
 
@@ -94,21 +97,21 @@ function showSlides(n) {
 
 //contactform
 
-function initialize() {
-  var mapOptions = {
-    center: new google.maps.LatLng(28.1823295, -82.352912),
-    zoom: 9,
-    mapTypeId: google.maps.MapTypeId.HYBRID,
-    scrollwheel: false,
-    draggable: false,
-    panControl: true,
-    zoomControl: true,
-    mapTypeControl: true,
-    scaleControl: true,
-    streetViewControl: true,
-    overviewMapControl: true,
-    rotateControl: true,
-  };
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
+// function initialize() {
+//   var mapOptions = {
+//     center: new google.maps.LatLng(28.1823295, -82.352912),
+//     zoom: 9,
+//     mapTypeId: google.maps.MapTypeId.HYBRID,
+//     scrollwheel: false,
+//     draggable: false,
+//     panControl: true,
+//     zoomControl: true,
+//     mapTypeControl: true,
+//     scaleControl: true,
+//     streetViewControl: true,
+//     overviewMapControl: true,
+//     rotateControl: true,
+//   };
+//   var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+// }
+// google.maps.event.addDomListener(window, 'load', initialize);
